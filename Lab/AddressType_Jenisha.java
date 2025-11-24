0a06ce409fc85d23ce918a2c54ed739eb6104d9f
Jenisha Byanju <146164136+Jenisha16@users.noreply.github.com>
2025-11-24 06:56:39 +0545

    Add files via upload

---
+++ Lab/AddressType_Jenisha.java
@@ -0,0 +1,27 @@
+package Lab;
+
+import java.net.*;
+
+public class AddressType_Jenisha {
+
+		    public static void main(String[] args) {
+		        try {
+		            InetAddress ip = InetAddress.getByName("255.234.180.23");
+
+		            System.out.println("IP Address: " + ip.getHostAddress());
+
+		            if (ip.isLoopbackAddress()) {
+		                System.out.println("Address Type: Loopback Address");
+		            } else if (ip.isMulticastAddress()) {
+		                System.out.println("Address Type: Multicast Address");
+		            } else if (ip.isSiteLocalAddress()) {
+		                System.out.println("Address Type: Private (Site Local) Address");
+		            } else {
+		                System.out.println("Address Type: Public Address");
+		            }
+
+		        } catch (Exception e) {
+		            System.out.println("Error: " + e.getMessage());
+		        }
+		    }
+		}
